30a98f1d2f3b2b74b8bea303d7c9d23db969f86e
root <mavaincee@gmail.com>
2024-07-28 14:34:49 +0300

    Your commit message

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,10 @@ git push production main
 echo "Changes have been pushed to the server."
 
 # Подключаемся к серверу и проверяем статус приложения
-sshpass -p "$SSH_PASSWORD" ssh root@185.218.0.64 << EOF
-cd /var/www/anidapha
-source venv/bin/activate
-pip install -r requirements.txt
-systemctl restart nginx
-echo "Server updated and restarted."
-EOF
+#sshpass -p "$SSH_PASSWORD" ssh root@185.218.0.64 << EOF
+#cd /var/www/anidapha
+#source venv/bin/activate
+#pip install -r requirements.txt
+#systemctl restart nginx
+#echo "Server updated and restarted."
+#EOF
