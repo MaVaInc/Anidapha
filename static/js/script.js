19a85b8cb4c7c53850d0eeeb76844a9703daf25c
ProClick <mavaincee@gmail.com>
2024-07-28 14:09:22 +0300

    Initial commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,13 @@
+// Обработчик события для кнопки toggle
 document.getElementById('toggle-button').addEventListener('change', function() {
     const extraButton = document.getElementById('extra-button');
     const circleButtons = document.querySelectorAll('.circle-button');
     if (this.checked) {
         extraButton.style.opacity = '1';
-        extraButton.style.transform = 'scale(1)';
+        extraButton.style.transform = 'scale(1.3)';
         circleButtons.forEach(button => {
             button.style.opacity = '1';
-            button.style.transform = 'scale(1)';
+            button.style.transform = 'scale(1.3)';
         });
     } else {
         extraButton.style.opacity = '0';
@@ -18,6 +19,7 @@ document.getElementById('toggle-button').addEventListener('change', function() {
     }
 });
 
+// Функция для отображения экрана
 function showScreen(screenId) {
     document.getElementById('main-screen').classList.add('hidden');
     document.getElementById('back-button').classList.add('active');
@@ -28,6 +30,7 @@ function showScreen(screenId) {
     document.getElementById(screenId).classList.add('active');
 }
 
+// Функция для возврата на главный экран
 function goBack() {
     document.getElementById('main-screen').classList.remove('hidden');
     document.getElementById('back-button').classList.remove('active');
@@ -36,3 +39,16 @@ function goBack() {
         screen.classList.remove('active');
     });
 }
+
+// Проверка наличия Telegram Web App и расширение его
+if (window.Telegram.WebApp) {
+    window.Telegram.WebApp.expand();
+}
+
+// Скрытие всех экранов и кнопки "Back" при загрузке страницы
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.screen').forEach(screen => {
+        screen.style.display = 'none';
+    });
+    document.getElementById('back-button').style.display = 'none';
+});
