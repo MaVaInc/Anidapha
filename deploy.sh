2c653d15bed3e7418bee411b7111919c938e37a2
root <mavaincee@gmail.com>
2024-07-28 14:39:26 +0300

    Your commit message

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git push production main
 echo "Changes have been pushed to the server."
 
 # Подключаемся к серверу и проверяем статус приложения
-sshpass -p "$SSH_PASSWORD" ssh root@185.218.0.64 << EOF
+sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no root@185.218.0.64 << EOF
 cd /var/www/anidapha
 source venv/bin/activate
 pip install -r requirements.txt
