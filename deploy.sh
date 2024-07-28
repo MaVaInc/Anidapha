b1121c0dc29dfb210ed0e63fd0bc73e0084f5dc1
root <mavaincee@gmail.com>
2024-07-28 14:37:19 +0300

    Your commit message

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,8 @@
 SSH_PASSWORD="Mavaincee202@"
 
 # Коммитим изменения
-git config --global user.email "mavaincee@gmail.com"
-echo "email."
 git add .
 git commit -m "$1"
-
 
 # Пушим изменения на сервер
 git push production main
